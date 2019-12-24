9f2e26b2cdbd82d482fb1c8d2dfab9effbba6cfa
Mike P. Sinn <m@quantimodo.com>
2019-12-23 22:35:53 -0600

    $this->getBaseClassName($model)

--- src/Coders/Model/Factory.php
+++ src/Coders/Model/Factory.php
@@ -568,7 +568,7 @@ class Factory
     private function formatBaseClasses(Model $model)
     {
         //return "use {$model->getBaseNamespace()}\\{$model->getClassName()} as {$this->getBaseClassName($model)};";
-        return "use {$model->getBaseNamespace()}\\{$model->getBaseClassName($model)};";
+        return "use {$model->getBaseNamespace()}\\{$this->getBaseClassName($model)};";
     }
 
     /**
